448eb81847ab42e9c2ee75c37ad9d9b233b680de
Nailahsfh <92776928+Nailahsfh@users.noreply.github.com>
2022-03-01 05:51:57 -0800

    Add files via upload

---
+++ P1-Variable dan Array/tugas_p1_Nailah Shofwah_0110121266_SI13/tugas1.php
@@ -0,0 +1,17 @@
+-Fungsi array_sort() digunakan untuk menyortir elemen-elemen dari array yang diindeks dalam ascending order (abjad untuk huruf dan angka untuk angka).
+<br>
+-Fungsi array_pop() berguna untuk menghapus elemen terakhir dari sebuah array.
+<br>
+-Fungsi array_push digunakan untuk menambahkan elemen baru ke akhir array.
+<br>
+-Fungsi array_shift() menghapus elemen pertama dari array, dan mengembalikan nilai elemen yang dihapus.
+<br>
+-Fungsi array_unshift() berguna untuk menyisipkan elemen baru ke array. Nilai array baru akan disisipkan di awal array.
+<br>
+1. Apa perbedaan variabel user dengan variabel system?
+<br>
+Perbedaan nya itu terletak. i admin system, Variable system bisa digunakan secara global oleh semua pengguna dan variable user itu hanya bisa digunakan oleh pengguna yang mendeklarasikan variable itu.
+<br>
+2. Apa persamaan variabel system dengan variabel konstan? 
+<br>
+Variabel system adalah suatu tempat untuk menampung data yang nilainya selalu berubah. Sedangkan, Konstanta adalah suatu tempat untuk menampung data yang nilainya selalu tetap dan tidak pernah berubah. Jadi persamaan variabel system dan variabel konstan adalah sama. suna untuk menampung data.
